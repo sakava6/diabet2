42cdea8b3b38f5885fab13d8acc934c0346babff
alexey <alexeyweb112gmail.com>
2022-01-31 12:39:07 +0200

    Initial Commit

---
+++ diabet/diabetApp.swift
@@ -0,0 +1,17 @@
+//
+//  diabetApp.swift
+//  diabet
+//
+//  Created by alexey on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct diabetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
